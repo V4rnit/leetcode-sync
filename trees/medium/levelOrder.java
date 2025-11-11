e5210b207bf4b99e476a983de7a182c8e61d8465
V4rnit <rawatvarnit122@gmail.com>
2025-11-10 22:37:53 -0500

    Level order traversal

---
+++ trees/medium/levelOrder.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class levelOrder{
+    public List<List<Integer>> levelOrderTraversal(TreeNode root){
+
+    }
+    public static void main(String args[]){
+
+    }
+}
--- trees/medium/validBST.java
+++ trees/medium/validBST.java
@@ -5,7 +5,7 @@ class TreeNode {
     TreeNode left;
     TreeNode right;
 
-    TreeNode(int val) {
+    public TreeNode(int val) {
         this.val = val;
         this.left = null;
         this.right = null;
